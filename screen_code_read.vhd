1bff3dc0f93b13dba201e9457ffa0768ea431459
LAPTOP-32MA3JE3\이세준 <ccc2191@gmail.com>
2021-05-26 17:19:06 +0900

    temporary code

--- screen_code_read.vhd
+++ screen_code_read.vhd
@@ -31,8 +31,53 @@ end screen_code_read;
 
 architecture Behavioral of screen_code_read is
 
+	type seg is array(0 to 31) of std_logic_vector(3 downto 0);
+	signal seg_file : seg;
+	signal cnt : std_logic_vector(2 downto 0);
+
 begin
 	-- test
+
+	process(FPGA_RSTB, CLK)
+	begin
+		if FPGA_RSTB = '0' then
+			for i in 0 to 31 loop
+				seg_file(i) <= "1000";							-- reset 입력 시 모든 segment를 8로 표시
+			end loop;
+		elsif CLK = '1' and CLK'event then
+			if push_dl = '0' then
+				seg_file(0) <= 
+				
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	process(FPGA_RSTB, CLK)										-- segment로 보내는 데이터
+	begin
+		if FPGA_RSTB = '0' then
+			cnt <= (others => '0');
+			seg_data_out <= '0';
+		elsif CLK = '1' and CLK'event then
+			if seg_w_enable = '1' then
+				seg_data <= seg_file(conv_integer(cnt));
+				seg_addr <= cnt;
+				seg_data_out <= '1';
+				if cnt = "101" then								-- segment 6자리까지 갔을 때
+					cnt <= (others => '0');
+				else
+					cnt <= cnt + 1;
+				end if;
+			else
+				seg_data_out <= '0';
+			end if;
+		end if;
+	end process;
 
 end Behavioral;
 
