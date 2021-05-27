0db6492647370f5bb44fcefba95985d36657d9a7
SK IM <isk0110@naver.com>
2021-05-27 18:05:32 +0900

    0527 복호화(시드구현 못하고 임의로 만든 암호화과정)

--- screen_read.vhd
+++ screen_read.vhd
@@ -35,10 +35,41 @@ end screen_read;
 
 architecture Behavioral of screen_read is
 
+signal r1_data_decode : STD_LOGIC_VECTOR (7 downto 0);
+signal syn_code : STD_LOGIC_VECTOR (7 downto 0);
+type segmentset is array( 0 to 6 ) of std_logic_vector( 3 downto 0 ); -- 2D array declare
+signal seg_reg_file : segmentset;
+
 begin
 
 
---skim 복호화
+r1_data_decode <= rl_data xor syn_code;
+
+process(FPGA_RSTB, CLK)
+begin
+	if FPGA_RSTB ='0' then
+		syn_code <= (others => '0');
+	elsif CLK='1' and CLK'event then
+		syn_code(7 downto 6) <= seg_reg_file(4)(1 downto 0);
+		syn_code(5) <= seg_reg_file(1)(0);
+		syn_code(4) <= seg_reg_file(6)(0);
+		syn_code(3) <= seg_reg_file(5)(0);
+		syn_code(2) <= seg_reg_file(2)(0);
+		syn_code(1 downto 0) <= seg_reg_file(3)(1 downto 0);
+	end if;
+end process;	
+
+
+--------------------------------- seg_reg_file 만들기
+
+process(CLK)
+Begin
+	if CLK='1' and CLK'event then
+		seg_reg_file (conv_integer (rc_addr(2 downto 0))) <= rc_data;
+	end if;
+end process;
+
+-----------------------------------여기서부터 read하기
 
 end Behavioral;
 
